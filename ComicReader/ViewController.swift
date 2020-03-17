403ac410792c85c47e8aecf0045b562c546edc87
Flavius Stan <flaviusns@outlook.com>
2020-03-17 15:40:22 +0100

    Initial Commit

---
+++ ComicReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComicReader
+//
+//  Created by Flavius Stan on 17/03/2020.
+//  Copyright © 2020 flaviusstan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
