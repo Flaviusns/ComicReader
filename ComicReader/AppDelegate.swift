f5ad4a8d6925d215eefc3166e89c5aeacd5d16d3
Flavius Stan <flaviusns@outlook.com>
2020-04-10 16:44:18 +0200

    Add open with app in iOS 12.4 too

--- ComicReader/AppDelegate.swift
+++ ComicReader/AppDelegate.swift
@@ -31,6 +31,46 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+            let name = url.lastPathComponent
+            let documentsPath = ComicFinder.getDocumentsDirectory()
+            let pathToSave = documentsPath.appendingPathComponent(name)
+            
+            if !FileManager.default.fileExists(atPath: pathToSave.path){
+                do {
+                    //try data?.write(to: documentsPath)
+                    try FileManager.default.moveItem(at: url, to: pathToSave)
+                    if !FileManager.default.fileExists(atPath: pathToSave.path){
+                        print("File not saved")
+                    }
+                    
+                    guard let superView = window?.rootViewController as? UINavigationController else{
+                        throw ComicError.ViewNotMain
+                    }
+                    /*
+                     let alert = UIAlertController(title: "Comic Added", message: "Please refresh the collection", preferredStyle: .alert)
+                     alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                     superView.present(alert, animated: true, completion: nil)*/
+                    guard let viewController = superView.viewControllers[0] as? ViewController else {
+                        throw ComicError.ViewNotMain
+                    }
+                    
+                    viewController.reloadData()
+                    
+                    let alert = UIAlertController(title: "Comic Added", message: "The comic is now in your collection", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                    viewController.present(alert, animated: true, completion: nil)
+                    
+                    
+                } catch {
+                    print("Unable to save the comic: " + error.localizedDescription)
+                    return false
+                }
+            }
+        return true
+    }
 
 }
 
